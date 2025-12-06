7d237b5d8c7190666e510dd881c1520ab5847f6e
Oliver Fida <oliver.fi99@gmail.com>
2025-12-06 12:16:12 +0100

    QOL Initials - Import from file

--- src/main/java/dev/xortix/suppautils/main/qol/initials/QolInitialsFeatureProvider.java
+++ src/main/java/dev/xortix/suppautils/main/qol/initials/QolInitialsFeatureProvider.java
@@ -1,7 +1,10 @@
 package dev.xortix.suppautils.main.qol.initials;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import dev.xortix.suppautils.main.Main;
 import dev.xortix.suppautils.main.base.FeatureProviderBase;
 import dev.xortix.suppautils.main.db.DBProvider;
 import dev.xortix.suppautils.main.log.Logger;
@@ -10,17 +13,24 @@ import dev.xortix.suppautils.main.shared.commands.CommandsManager;
 import dev.xortix.suppautils.main.shared.commands.CustomSuppaCommand;
 import dev.xortix.suppautils.main.shared.commands.SuppaCommand;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.command.argument.GameProfileArgumentType;
 import net.minecraft.server.PlayerConfigEntry;
 import net.minecraft.text.Text;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
@@ -39,6 +49,7 @@ public class QolInitialsFeatureProvider extends FeatureProviderBase {
     @Override
     public void init() {
         initFromDb();
+        new Thread(this::importFromFile).start();
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> PlayerListManager.updatePlayerList());
         CommandsManager.addToRegistrationList(new SuppaCommand(SuppaCommand.TYPE.ENABLE, this));
@@ -91,6 +102,27 @@ public class QolInitialsFeatureProvider extends FeatureProviderBase {
                         })
                 )
         ));
+        CommandsManager.addToRegistrationList(new CustomSuppaCommand(this, literal("import")
+                .requires(source -> source.hasPermissionLevel(2))
+                .executes(ctx -> {
+                    ctx.getSource().sendFeedback(() -> Text.literal("§8Trying to import initials. Please wait..."), false);
+                    int result = importFromFile();
+
+                    switch (result) {
+                        case 200:
+                            ctx.getSource().sendFeedback(() -> Text.literal("§aInitials updated from file."), false);
+                            break;
+                        case 400:
+                            ctx.getSource().sendFeedback(() -> Text.literal("§cImport failed! Please see log."), false);
+                            break;
+                        case 404:
+                            ctx.getSource().sendFeedback(() -> Text.literal("§cNo file found for import."), false);
+                            break;
+                    }
+
+                    return Command.SINGLE_SUCCESS;
+                })
+        ));
     }
 
     @Override
@@ -123,6 +155,72 @@ public class QolInitialsFeatureProvider extends FeatureProviderBase {
             }
         } catch (Exception ex) {
             Logger.log(Logger.LogCategory.INITIALS, Logger.LogType.ERROR, ex.getMessage());
+        }
+    }
+
+    private int importFromFile() {
+        Path filePath = FabricLoader.getInstance().getConfigDir().resolve("suppautils-initials.csv");
+        Path filePathAfter = FabricLoader.getInstance().getConfigDir().resolve("suppautils-initials_done.csv");
+        if (!Files.exists(filePath)) return 404;
+
+        try {
+            List<String> lines = Files.readAllLines(filePath.toAbsolutePath());
+
+            int trys = 0;
+            while (Main.SERVER == null && trys < 120) {
+                trys++;
+                Thread.sleep(1000);
+            }
+            if (trys == 120) throw new Exception("Server took too long to startup");
+
+            for (String line : lines) {
+                try {
+                    String[] parts = line.split(";");
+
+                    UUID uuid = getPlayerUuidFromApi(parts[0]);
+
+                    InitialsEntry currentInitials = Initials.get(uuid);
+                    updateInitials(uuid, currentInitials, parts[1]);
+                } catch (Exception ex) {
+                    Logger.log(Logger.LogCategory.INITIALS, Logger.LogType.ERROR, ex.getMessage());
+                }
+            }
+
+            Files.move(filePath, filePathAfter, StandardCopyOption.REPLACE_EXISTING);
+
+            Logger.log(Logger.LogCategory.INITIALS, Logger.LogType.INFO, "Imported from file");
+        } catch (Exception ex) {
+            Logger.log(Logger.LogCategory.INITIALS, Logger.LogType.ERROR, ex.getMessage());
+            return 400;
+        }
+
+        return 200;
+    }
+
+    private UUID getPlayerUuidFromApi(String username) throws Exception {
+        try {
+            URI apiUri = URI.create("https://api.mojang.com/users/profiles/minecraft/" + username);
+            URL apiUrl = apiUri.toURL();
+
+            HttpURLConnection conn = (HttpURLConnection) apiUrl.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(3000);
+            conn.setReadTimeout(3000);
+
+            int responseCode = conn.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) throw new Exception("HTTP error code " + responseCode);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String response = reader.lines().collect(Collectors.joining());
+            reader.close();
+
+            JsonObject json = JsonParser.parseString(response).getAsJsonObject();
+            String uuid = json.get("id").getAsString();
+
+            return UUID.fromString(uuid.replaceFirst("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})",
+                    "$1-$2-$3-$4-$5"));
+        } catch (Exception ex) {
+            throw new Exception("Fetching UUID from Mojang API failed for \"" + username + "\": " + ex.getMessage());
         }
     }
 
